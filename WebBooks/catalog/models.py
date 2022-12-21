d35c064452f23469b98b732f3a6a37445a2df443
denis <denis.falin@protonmail.com>
2022-12-21 21:21:10 +0500

    Reset migration

--- WebBooks/catalog/models.py
+++ WebBooks/catalog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class MyModelName(models.Model):
+    my_field_name = models.CharField(max_length=20,
+                                     help_text="Не более 20 символов")
+
+
+class Meta:
+    ordering = ["-my_field_name"]
+
+
+def get_absolute_url(self):
+    return reverse('model-detail-view', args=[str(self.id)])
+
+
+def __str__(self):
+    return self.field_name
